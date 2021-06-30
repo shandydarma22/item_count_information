6e54d1cdfd2346e2acbfd430287b1f3541d524f0
shandydarma22 <83050623+shandydarma22@users.noreply.github.com>
2021-06-30 21:18:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,99 @@
+<html>
+    <head>
+    <title>Document</title>
+        <!-- Style Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        
+        <!-- Main Style -->
+        <link rel="stylesheet" href="../../assets/css/style.css">
+    </head>
+    
+    <body>
+        <nav class="navheader">
+            <img src="../../assets/image/Logo.svg" alt="" width="" height="">
+            <strong>Item Count Information</strong>
+        </nav>
+
+        <div class="row justify-content-md-center">
+            <div class="col-2 menubar">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#"><img src="../../assets/image/category.svg"/> Category</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Color</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tags</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-4 table1">
+                <h3>Presentation</h3>
+
+                <table class="table">
+                    <thead>
+                        <tr class="trheader">
+                            <th scope="col">Category</th>
+                            <th scope="col">Count</th>
+                            <th scope="col">View</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <td>Larry the Bird</td>
+                            <td>Thornton</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="col-4 table1">
+                <h3>Infographic</h3>
+
+                <table class="table">
+                    <thead>
+                        <tr class="trheader">
+                            <th scope="col">Category</th>
+                            <th scope="col">Count</th>
+                            <th scope="col">View</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <td>Larry the Bird</td>
+                            <td>Thornton</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            
+            </div>           
+        </div>
+    </body>
+</html>
